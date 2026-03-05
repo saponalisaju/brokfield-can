ac784fdecaaf256d603f203942b387adaee2347d
saponalisaju <saponalibpl12486@gmail.com>
2026-03-05 17:44:30 +0600

    ewqw3

--- frontend/src/components/component1/Home1.js
+++ frontend/src/components/component1/Home1.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./home1.css";
 import "../../assets/styles/main.css";
 import axios from "axios";
